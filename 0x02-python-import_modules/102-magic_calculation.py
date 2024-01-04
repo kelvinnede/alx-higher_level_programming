0cc0bbdf4a224da2cf35e7a5137b05c1ce7dbc43
Kelvin Nede <kelvinnede@gmail.com>
2024-01-04 08:27:36 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+
 def magic_calculation(a, b):
     c = 0
+
     if a < b:
+        from magic_calculation_102 import add
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
     else:
+        from magic_calculation_102 import sub
         c = sub(a, b)
+
     return c
