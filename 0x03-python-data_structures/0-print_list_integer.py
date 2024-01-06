6ff1beb66355d3f43c841416eaa55a5b0290562b
Kelvin Nede <kelvinnede@gmail.com>
2024-01-06 00:06:34 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,6 @@ def print_list_integer(my_list=[]):
 
 
 # Example usage
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
