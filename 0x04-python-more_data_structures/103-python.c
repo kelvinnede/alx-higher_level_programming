2b984b4d74582c33db20bb2c90133642d87bcfe7
Kelvin Nede <kelvinnede@gmail.com>
2024-01-10 04:57:23 -0800

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -9,19 +9,21 @@ void print_python_bytes(PyObject *p);
  */
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t size, i;
-    PyObject *element;
+	Py_ssize_t size, i;
+	PyObject *element;
 
-    size = PyList_Size(p);
-    printf("[*] Python list info\n[*] Size of the Python List = %ld\n[*] Allocated = %ld\n", size, ((PyListObject *)p)->allocated);
+	size = PyList_Size(p);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-    for (i = 0; i < size; i++)
-    {
-        element = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
-        if (PyBytes_Check(element))
-            print_python_bytes(element);
-    }
+	for (i = 0; i < size; i++)
+	{
+		element = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
+		if (PyBytes_Check(element))
+			print_python_bytes(element);
+	}
 }
 
 /**
@@ -30,18 +32,18 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-    Py_ssize_t size, i;
-    char *string;
+	Py_ssize_t size, i;
+	char *string;
 
-    printf("[.] bytes object info\n");
-    size = PyBytes_Size(p);
-    printf("  size: %ld\n", size);
+	printf("[.] bytes object info\n");
+	size = PyBytes_Size(p);
+	printf("  size: %ld\n", size);
 
-    string = PyBytes_AsString(p);
-    printf("  trying string: %s\n", string);
+	string = PyBytes_AsString(p);
+	printf("  trying string: %s\n", string);
 
-    printf("  first 10 bytes: ");
-    for (i = 0; i < size && i < 10; i++)
-        printf("%02hhx ", string[i]);
-    printf("\n");
+	printf("  first 10 bytes: ");
+	for (i = 0; i < size && i < 10; i++)
+		printf("%02hhx ", string[i]);
+	printf("\n");
 }
