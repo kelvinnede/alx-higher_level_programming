3af1cf3f3b355e0112edd8f281644daded1c51b4
Kelvin Nede <kelvinnede@gmail.com>
2024-01-11 05:58:52 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,13 +13,12 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 /* Function prototype for print_dlistint */
 size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
-
