7bb230a58dd67651a278c1e7ec01d1c98dbc038d
Kelvin Nede <kelvinnede@gmail.com>
2024-01-22 06:16:42 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,9 +8,9 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         print()
-        return count
-    except (IndexError, TypeError):
-        print()
+    except IndexError:
+        pass
+    finally:
         return count
 
 
