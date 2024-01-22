f99a08630851cc45efd8ad4e152fff9bc6b027d2
Kelvin Nede <kelvinnede@gmail.com>
2024-01-22 12:48:11 -0800

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            element_1 = my_list_1[i] if i < len(my_list_1) else 0
-            element_2 = my_list_2[i] if i < len(my_list_2) else 1  # Avoid division by zero
-            division_result = element_1 / element_2
-        except TypeError:
-            print("wrong type")
-            division_result = 0
+            division_result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
+            division_result = 0
+        except TypeError:
+            print("wrong type")
             division_result = 0
         except IndexError:
             print("out of range")
@@ -19,18 +16,3 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             result.append(division_result)
     return result
-
-
-# Test the function
-if __name__ == "__main__":
-    my_l_1 = [10, 8, 4]
-    my_l_2 = [2, 4, 4]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
-
-    print("--")
-
-    my_l_1 = [10, 8, 4, 4]
-    my_l_2 = [2, 0, "H", 2, 7]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
