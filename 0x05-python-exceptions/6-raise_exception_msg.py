e423d2cc53e055e7e5458904ce19c0d7e10f1fa6
Kelvin Nede <kelvinnede@gmail.com>
2024-01-22 08:36:19 -0800

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
+
+# Uncomment the next three lines if you want to run this script independently
+# try:
+#     raise_exception_msg("C is fun")
+# except NameError as ne:
+#     print(ne)
