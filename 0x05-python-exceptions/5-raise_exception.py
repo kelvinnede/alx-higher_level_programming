5b3219b60a9a4c8176db83568cd40e8c032307d3
Kelvin Nede <kelvinnede@gmail.com>
2024-01-22 08:31:46 -0800

    5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    raise TypeError("Exception raised")
+
+# Uncomment the next two lines if you want to run this script independently
+# try:
+#     raise_exception()
+# except TypeError as te:
+#     print("Exception raised")
