d7ca8d2e9832ffe70889262a9cdb3fefdeca1b14
Kelvin Nede <kelvinnede@gmail.com>
2024-01-23 05:33:46 -0800

    100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+"""
+This module contains classes for a singly linked list.
+"""
+
+
+class Node:
+    """
+    This class defines a node of a singly linked list.
+    """
+
+    def __init__(self, data, next_node=None):
+        """
+        Initializes a new instance of the Node class.
+
+        Args:
+            data: The data for the node.
+            next_node: The next node in the linked list.
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """
+        Getter method for retrieving the data attribute.
+
+        Returns:
+            int: The data of the node.
+        """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """
+        Setter method for setting the value of the data attribute.
+
+        Args:
+            value: The new data to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+        """
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """
+        Getter method for retrieving the next_node attribute.
+
+        Returns:
+            Node: The next node in the linked list.
+        """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """
+        Setter method for setting the value of the next_node attribute.
+
+        Args:
+            value: The new next_node to set.
+
+        Raises:
+            TypeError: If value is not a Node or None.
+        """
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    """
+    This class defines a singly linked list.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the SinglyLinkedList class.
+        """
+        self.head = None
+
+    def sorted_insert(self, value):
+        """
+        Inserts a new Node into the correct sorted position in the list
+        (increasing order).
+
+        Args:
+            value: The value to insert.
+        """
+        new_node = Node(value)
+        if self.head is None or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next_node is not None and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
+
+    def __str__(self):
+        """
+        Returns a string representation of the linked list.
+
+        Returns:
+            str: The string representation of the linked list.
+        """
+        result = []
+        current = self.head
+        while current is not None:
+            result.append(str(current.data))
+            current = current.next_node
+        return '\n'.join(result)
+
+
+if __name__ == "__main__":
+    SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
+
+    sll = SinglyLinkedList()
+    sll.sorted_insert(2)
+    sll.sorted_insert(5)
+    sll.sorted_insert(3)
+    sll.sorted_insert(10)
+    sll.sorted_insert(1)
+    sll.sorted_insert(-4)
+    sll.sorted_insert(-3)
+    sll.sorted_insert(4)
+    sll.sorted_insert(5)
+    sll.sorted_insert(12)
+    sll.sorted_insert(3)
+    print(sll)
