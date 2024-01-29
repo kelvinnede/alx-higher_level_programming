6dbce86796362bbcb603d3851595f882e0b4e29c
Kelvin Nede <kelvinnede@gmail.com>
2024-01-29 02:36:03 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -49,77 +49,77 @@ class Rectangle:
         else:
             self.__width = value
 
-        @property
-        def height(self):
-            """
-            Getter method for the height attribute.
+    @property
+    def height(self):
+        """
+        Getter method for the height attribute.
 
-            Returns:
-                int: The height of the rectangle.
-            """
-            return self.__height
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
-            Setter method for the height attribute.
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for the height attribute.
 
-            Args:
-                value (int): The value to set as the height.
+        Args:
+            value (int): The value to set as the height.
 
-            Raises:
-                TypeError: If the value is not an integer.
-                ValueError: If the value is less than 0.
-            """
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
-        def area(self):
-            """
-            Calculates the area of the rectangle.
+    def area(self):
+        """
+        Calculates the area of the rectangle.
 
-            Returns:
-                int: The area of the rectangle.
-            """
-            return self.__width * self.__height
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
 
-        def perimeter(self):
-            """
-            Calculates the perimeter of the rectangle.
+    def perimeter(self):
+        """
+        Calculates the perimeter of the rectangle.
 
-            Returns:
-                int: The perimeter of the rectangle.
-            """
-            return 2 * (self.__width + self.__height)
+        Returns:
+            int: The perimeter of the rectangle.
+        """
+        return 2 * (self.__width + self.__height)
 
-        def __str__(self):
-            """
-            Returns a string representation of the rectangle.
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
 
-            Returns:
-                str: String representation of the rectangle using '#'.
-            """
-            if self.__width == 0 or self.__height == 0:
-                return ""
-            return "\n".join(
-                ["#" * self.__width for _ in range(self.__height)]
-            )
+        Returns:
+            str: String representation of the rectangle using '#'.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(
+            ["#" * self.__width for _ in range(self.__height)]
+        )
 
-        def __repr__(self):
-            """
-            Returns a string representation of the rectangle for reproduction.
+    def __repr__(self):
+        """
+        Returns a string representation of the rectangle for reproduction.
 
-            Returns:
-                str: String representation of the rectangle.
-            """
-            return "Rectangle({}, {})".format(self.__width, self.__height)
+        Returns:
+            str: String representation of the rectangle.
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
-        def __del__(self):
-            """
-            Prints a message when an instance of Rectangle is deleted.
-            """
-            print("Bye rectangle...")
+    def __del__(self):
+        """
+        Prints a message when an instance of Rectangle is deleted.
+        """
+        print("Bye rectangle...")
