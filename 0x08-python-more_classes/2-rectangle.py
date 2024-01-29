3ad58206b63064f3386a6541ca7dbe2f6569e252
Kelvin Nede <kelvinnede@gmail.com>
2024-01-29 01:11:39 -0800

    2-main.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,88 +11,87 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """
-    Initializes a new instance of the Rectangle class.
+        Initializes a new instance of the Rectangle class.
 
-    Args:
-    width (int): The width of the rectangle.
-    height (int): The height of the rectangle.
-    """
-    self.width = width
-    self.height = height
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """
-    Getter method for the width attribute.
+        Getter method for the width attribute.
 
-    Returns:
-    int: The width of the rectangle.
-    """
-    return self.__width
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
 
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for the width attribute.
 
-@width.setter
-def width(self, value):
-    """
-    Setter method for the width attribute.
+        Args:
+            value (int): The value to set as the width.
 
-    Args:
-    value (int): The value to set as the width.
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
-    Raises:
-    TypeError: If the value is not an integer.
-    ValueError: If the value is less than 0.
-    """
-    if not isinstance(value, int):
-        raise TypeError("width must be an integer")
-    elif value < 0:
-        raise ValueError("width must be >= 0")
-    else:
-        self.__width = value
-
-        @property
-        def height(self):
-            """
+    @property
+    def height(self):
+        """
         Getter method for the height attribute.
 
         Returns:
-        int: The height of the rectangle.
+            int: The height of the rectangle.
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-    Setter method for the height attribute.
+        Setter method for the height attribute.
 
-    Args:
-    value (int): The value to set as the height.
+        Args:
+            value (int): The value to set as the height.
 
-    Raises:
-    TypeError: If the value is not an integer.
-    ValueError: If the value is less than 0.
-    """
-    if not isinstance(value, int):
-        raise TypeError("height must be an integer")
-    elif value < 0:
-        raise ValueError("height must be >= 0")
-    else:
-        self.__height = value
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
-        def area(self):
-            """
+    def area(self):
+        """
         Calculates the area of the rectangle.
 
         Returns:
-        int: The area of the rectangle.
+            int: The area of the rectangle.
         """
         return self.__width * self.__height
 
     def perimeter(self):
         """
-    Calculates the perimeter of the rectangle.
+        Calculates the perimeter of the rectangle.
 
-    Returns:
-    int: The perimeter of the rectangle.
-    """
-    return 2 * (self.__width + self.__height)
+        Returns:
+            int: The perimeter of the rectangle.
+        """
+        return 2 * (self.__width + self.__height)
