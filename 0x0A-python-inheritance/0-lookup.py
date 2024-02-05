7bca877992b9aa5ae1885c542908222e881f9787
Kelvin Nede <kelvinnede@gmail.com>
2024-02-05 03:35:59 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -19,10 +19,9 @@ if __name__ == "__main__":
 
     class MyClass2(object):
         my_attr1 = 3
-
         def my_meth(self):
             pass
 
-    print(lookup(MyClass1))
-    print(lookup(MyClass2))
-    print(lookup(int))
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
