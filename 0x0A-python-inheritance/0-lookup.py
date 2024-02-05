9f63eadd72e0f0fce8d9b64716a42940bdb790c0
Kelvin Nede <kelvinnede@gmail.com>
2024-02-05 02:19:19 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+    obj (object): The object for which to retrieve attributes and methods.
+
+    Returns:
+    list: A list containing the names of attributes and methods of the object.
+    """
+    return dir(obj)
+
+
+if __name__ == "__main__":
+    class MyClass1(object):
+        pass
+
+    class MyClass2(object):
+        my_attr1 = 3
+
+        def my_meth(self):
+            pass
+
+    print(lookup(MyClass1))
+    print(lookup(MyClass2))
+    print(lookup(int))
