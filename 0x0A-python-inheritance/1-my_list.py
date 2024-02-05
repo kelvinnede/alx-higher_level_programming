1a2c1a9ff7967f56b29762d6432523cc2d66a942
Kelvin Nede <kelvinnede@gmail.com>
2024-02-05 14:02:03 -0800

    2nd

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
+"""defines class MyList"""
+
 
 class MyList(list):
-    """
-    MyList class inherits from list and adds a print_sorted method.
-    """
+    """class that inherits from list
+    with public instance method to print sorted list"""
 
     def print_sorted(self):
-        """
-        Prints the list, but sorted in ascending order.
-        """
         print(sorted(self))
-
-        if __name__ == "__main__":
-            MyList = __import__('1-my_list').MyList
-
-            my_list = MyList()
-            my_list.append(1)
-            my_list.append(4)
-            my_list.append(2)
-            my_list.append(3)
-            my_list.append(5)
-
-            print(my_list)
-            my_list.print_sorted()
-            print(my_list)
