40e1e114656c60f785542cb74250abb1dca03653
Kelvin Nede <kelvinnede@gmail.com>
2024-02-06 07:35:26 -0800

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module for generating Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the nth row.
+    Args:
+        n (int): The number of rows to generate.
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
