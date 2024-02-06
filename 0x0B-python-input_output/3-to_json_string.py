c7c5dae20660cbad185b7f6b6a8a59feeb050206
Kelvin Nede <kelvinnede@gmail.com>
2024-02-06 05:55:13 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for converting an object to a JSON string.
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+    Args:
+        my_obj: The object to convert to JSON.
+    Returns:
+        str: The JSON representation of the object.
+    """
+    import json
+    return json.dumps(my_obj)
