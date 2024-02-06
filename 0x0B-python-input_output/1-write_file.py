0c122983994059c52072d9399996c7e2c3801a44
Kelvin Nede <kelvinnede@gmail.com>
2024-02-06 05:17:45 -0800

    1-main.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for writing a string to a text file and returning the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file and returns the number of characters written.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The string to write to the file.
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return file.write(text)
