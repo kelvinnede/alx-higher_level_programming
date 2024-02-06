17449863c7cec67885846a3223dba3b42e457439
Kelvin Nede <kelvinnede@gmail.com>
2024-02-06 05:52:08 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,7 +7,8 @@ and returning the number of characters added.
 
 def append_write(filename="", text=""):
     """
-    Appends a string to the end of a text file and returns the number of characters.
+    Appends a string to the end of a text file and
+    returns the number of characters.
     Args:
         filename (str): The name of the file to append to.
         text (str): The string to append to the file.
