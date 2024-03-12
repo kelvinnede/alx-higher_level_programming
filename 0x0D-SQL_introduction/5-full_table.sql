8c359fc8350cca5ba2365327e81a089c8f6af3e5
Kelvin Nede <kelvinnede@gmail.com>
2024-03-12 10:05:44 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,6 @@
 -- Script to print full description of table first_table
-SELECT CONCAT('Table: ', table_name, '\n', 'Create Table: ', create_statement) AS Description
+SELECT CONCAT('Table: ', table_name, '\n', 
+              'Create Table: ', create_statement) AS Description
 FROM information_schema.tables
 WHERE table_schema = 'hbtn_0c_0'
 AND table_name = 'first_table';
