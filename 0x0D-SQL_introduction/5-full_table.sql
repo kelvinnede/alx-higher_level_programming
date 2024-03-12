d52ab83754052c54be3ecde9c4b4a6c3596d0e80
Kelvin Nede <kelvinnede@gmail.com>
2024-03-12 10:11:51 -0700

    6-list_values.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
--- Script to print full description of table first_table
-SELECT CONCAT('Table: ', table_name, '\n', 
-              'Create Table: ', create_statement) AS Description
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0'
-AND table_name = 'first_table';
+-- Prints the full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- Script to list all rows of table first_table
-SELECT * FROM hbtn_0c_0.first_table;
+-- Lists all rows of the table first_table.
+SELECT * FROM `first_table`;
