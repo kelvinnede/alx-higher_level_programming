a7b4322f9f2ce45bb937a9b432de8fd579e8054e
Kelvin Nede <kelvinnede@gmail.com>
2024-03-13 06:32:11 -0700

    103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- Selecting all genres except those linked to the show Dexter
+SELECT name
+FROM tv_genres
+WHERE name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
