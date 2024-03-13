cd2590e3a8d9be3926f5cd2f1e8cca3762f9253d
Kelvin Nede <kelvinnede@gmail.com>
2024-03-12 22:42:09 -0700

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Granting privileges to user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Granting privileges to user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
