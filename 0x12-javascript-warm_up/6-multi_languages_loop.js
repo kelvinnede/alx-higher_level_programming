b3ea5bff44c1b8fd4a0ee9f9595455e635759c95
Kelvin Nede <kelvinnede@gmail.com>
2024-04-08 15:21:47 -0700

    13-add.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,6 @@ const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 const arrayLength = myLines.length;
 let i = 0;
 while (i < arrayLength) {
-	  console.log(myLines[i]);
-	  i++;
+  console.log(myLines[i]);
+  i++;
 }
