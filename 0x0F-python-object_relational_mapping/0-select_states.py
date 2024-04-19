9273d8105828c016787e761542f0c8e0d330f98b
Kelvin Nede <kelvinnede@gmail.com>
2024-04-19 03:44:24 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,14 @@
-import MySQLdb
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa
+the script takes 3 arguments
+"""
+
 import sys
+import MySQLdb
 
-if len(sys.argv) != 4:
-    print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-    sys.exit(1)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-try:
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-except MySQLdb.Error as e:
-    print("Error connecting to MySQL database:", e)
-    sys.exit(1)
-
-cursor = db.cursor()
-
-query = "SELECT * FROM states ORDER BY id ASC;"
-
-try:
-    cursor.execute(query)
-except MySQLdb.Error as e:
-    print("Error executing SQL query:", e)
-    db.close()
-    sys.exit(1)
-
-results = cursor.fetchall()
-for row in results:
-    print(row)
-
-cursor.close()
-db.close()
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,12 @@
 #!/usr/bin/python3
-import MySQLdb
+""" Filter states """
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-        if len(sys.argv) != 4:
-                    print("Usage: {} username password database".format(sys.argv[0]))
-                            sys.exit(1)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-                                username, password, database = sys.argv[1:]
-
-                                    # Connect to MySQL server
-                                        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-                                            # Create a cursor object
-                                                cursor = db.cursor()
-
-                                                    # Execute SQL query to select states starting with 'N'
-                                                        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-                                                            # Fetch all rows
-                                                                rows = cursor.fetchall()
-
-                                                                    # Display results
-                                                                        for row in rows:
-                                                                                    print(row)
-
-                                                                                        # Close cursor and connection
-                                                                                            cursor.close()
-                                                                                                db.close()
-
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
